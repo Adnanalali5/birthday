3c61f34895b945ab5458a3a7ce8a22ac1b73c0ad
Adnanalali5 <adnanalali5@gmail.com>
2021-02-23 10:51:06 +0100

    Updates

---
+++ precache-manifest.3be26b5111e8b08bf2a7732048c0b0c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc3929c60cba518a1524cd4e948f523",
+    "url": "/birthdayReminder/index.html"
+  },
+  {
+    "revision": "242c2e83121d1075eabd",
+    "url": "/birthdayReminder/static/css/main.fedc0aa0.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthdayReminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthdayReminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242c2e83121d1075eabd",
+    "url": "/birthdayReminder/static/js/main.5730a3c3.chunk.js"
+  },
+  {
+    "revision": "7b358b8c711e01f2f05d",
+    "url": "/birthdayReminder/static/js/runtime-main.60db2f69.js"
+  }
+]);
